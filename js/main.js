d1ba3aa4e4bcbd0d2cecf1becae7ed005b25f80e
mrforgEstudios <134918828+mrforgEstudios@users.noreply.github.com>
2024-07-05 21:36:36 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function mc() {
+    window.open('https://youtu.be/Ev5g_iFGpno?si=p1HmgWjXfr3Qtaiu');
+}
+function rickroll() {
+    window.open('https://discord.gg/FWY4HDHvJJ')
+}
+function bont() {
+    window.location.href= 'tabs/video.html'
+}
